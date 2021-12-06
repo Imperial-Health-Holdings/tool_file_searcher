c8f34f02b84d81cbb4ce0cc9b18cd33283394104
Henry Cheng <Henry.Cheng@imperialhealthholdings.com>
2021-12-06 14:41:43 -0800

    add feature: mailer by condition

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import os
-import pandas as pd
 from datetime import datetime
 from mailer import mailer
-
 import configparser
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+config_mailer = configparser.ConfigParser()
+config_mailer.read('./mailer/config_mailer.ini')
+
 
 # ==== initialization ====
 path_directory = config['PATH']['path_directory']
@@ -19,6 +26,20 @@ file2 = config['FILENAME']['file2_prefix'] + f'_{year_month}.xlsx'
 path_file1 = os.path.join(path_directory, file1)
 path_file2 = os.path.join(path_directory, file2)
 
+
+# ==== email parameters ====
+char_month = datetime.now().strftime('%b')
+
+# SSRS ran successfully
+recipient = config_mailer['MAILER_SUCCESS']['recipient']
+subject = config_mailer['MAILER_SUCCESS']['subject'] + ' ' + char_month
+text = config_mailer['MAILER_SUCCESS']['text']
+
+# SSRS failed
+recipient_failed = config_mailer['MAILER_FAILED']['recipient']
+subject_failed = config_mailer['MAILER_FAILED']['subject'] + ' ' + char_month
+
+
 # ==== look for files in directory ====
 list_files = [path_file1, path_file2]
 list_none_exist = []
@@ -29,3 +50,9 @@ for file in list_files:
         list_none_exist.append(file)
 
 # mailer with conditions
+if len(list_none_exist) > 0:
+    text_header = 'SSRS export failed with following files missing: \n'
+    text_failed = text_header + '\n'.join(list_none_exist)
+    mailer.send_report_notification(recipient_failed, subject_failed, text_failed)
+else:
+    mailer.send_report_notification(recipient, subject, text)
