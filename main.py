0309e291340589d833f8dc804c521e70408dac1e
Henry Cheng <Henry.Cheng@imperialhealthholdings.com>
2021-12-06 14:04:39 -0800

    add file check in path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+from mailer import mailer
 
 import configparser
 config = configparser.ConfigParser()
@@ -12,13 +13,19 @@ year_month = str(datetime.now().year) + str(datetime.now().month)
 file1 = config['FILENAME']['file1_prefix'] + f'_{year_month}.xlsx'
 file2 = config['FILENAME']['file2_prefix'] + f'_{year_month}.xlsx'
 
+path_file1 = os.path.join(path_directory, file1)
+path_file2 = os.path.join(path_directory, file2)
+
 # look for files in directory
-if os.path.exists(os.path.join(path_directory, file1)):
+file1_exist = os.path.exists(path_file1)
+file2_exist = os.path.exists(path_file2)
+
+if file1_exist:
     print(f'{file1} exists.')
 else:
     print(f'{file1} does not exist')
 
-if os.path.exists(os.path.join(path_directory, file2)):
+if file2_exist:
     print(f'{file2} exists.')
 else:
     print(f'{file2} does not exist') 
